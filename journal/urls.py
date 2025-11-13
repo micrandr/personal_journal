f9de9ec10e59dfc7837a10aec043cf3967445c73
micrandr <micrandr@gmail.com>
2025-11-13 20:23:40 +0300

    feat: add entry page / login / logout

--- journal/urls.py
+++ journal/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add/', views.add_entry, name='add_entry'),
+    path('signup/', views.signup, name='signup'),  # signup route
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- journal/views.py
+++ journal/views.py
@@ -1,6 +1,34 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
+from .forms import JournalEntryForm
 from .models import JournalEntry
+
+
 
 def home(request):
     entries = JournalEntry.objects.all().order_by('-created_at')
     return render(request, 'journal/home.html', {'entries': entries})
+
+@login_required
+def add_entry(request):
+    if request.method == 'POST':
+        form = JournalEntryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = JournalEntryForm()
+    return render(request, 'journal/add_entry.html', {'form': form})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # automatically log the user in
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
